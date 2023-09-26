18c6c6285621a492bef37864052fc5580ea6e08d
Kristian <49764796+kristianka@users.noreply.github.com>
2023-09-26 21:10:02 +0300

    Revert "add files"
    This reverts commit 33b1aeaceb7d20c5c4dd16fdb75cdd17518a9be3.

--- viikkoharjoitukset_1/constraint_layout/app/src/main/java/com/example/constraint_layout/MainActivity.java
+++ viikkoharjoitukset_1/constraint_layout/app/src/main/java/com/example/constraint_layout/MainActivity.java
@@ -2,22 +2,50 @@ package com.example.constraint_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button[] buttons;
+    private boolean[] buttonStates;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttons = new Button[]{
+            findViewById(R.id.button1),
+            findViewById(R.id.button2),
+            findViewById(R.id.button3),
+            findViewById(R.id.button4)
+        };
+
+        // button states array
+        buttonStates = new boolean[buttons.length];
+
+        for (int i = 0; i < buttons.length; i++) {
+            final int buttonIndex = i;
+            // event listeners for every button
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    toggleButtonBackground(buttons[buttonIndex], buttonIndex);
+                }
+            });
+        }
     }
-
-    public void buttonClicked(View view) {
-        Button button = (Button)view;
-        button.setBackgroundColor();
-
+    private void toggleButtonBackground(Button button, int index) {
+        int newColor;
+        if (buttonStates[index]) {
+            newColor = getResources().getColor(R.color.normalColor);
+        } else {
+            newColor = getResources().getColor(R.color.pressedColor);
+        }
+        button.setBackgroundColor(newColor);
+        buttonStates[index] = !buttonStates[index];
     }
 }
